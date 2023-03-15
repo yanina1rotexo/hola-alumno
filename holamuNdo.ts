4f62240ceb047b45121f75991231ce4ba6af9ca9
yanina1rotexo <yanina.rotexo@gmail.com>
2023-03-15 19:51:58 -0300

    cree hola mundo

--- holamuNdo.ts
+++ holamuNdo.ts
@@ -1,1 +1,6 @@
-console.log("holamuNdo"); 
+console.log ("instrucciones");
+console.log ("paso 1");
+console.log ("paso 2");
+console.log ("paso 3");
+console.log ("paso 4");
+console.log ("paso 5");
